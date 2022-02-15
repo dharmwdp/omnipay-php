bf84b9ec1c5e8198838bdeba2bfe1beaf7d59b61
Dharmraj Kumhar <98737413+dharmwdp@users.noreply.github.com>
2022-02-15 10:55:21 +0530

    Add files via upload

--- src/EncryptDecrypt.php
+++ src/EncryptDecrypt.php
@@ -16,17 +16,17 @@ class EncryptDecrypt
                 $output = openssl_encrypt($string, $encrypt_method, $secret_key, OPENSSL_RAW_DATA, $secret_iv);
                 $output = bin2hex($output);
                 $code = 200;            
-                $content =  ['status'=>$code,  "httpStatus"=> $code,"codeDescription"=> __("Success"),  'apiResponse'=>$output];
+                $content =  ['status'=>$code,  "httpStatus"=> $code,"codeDescription"=> "Success",  'apiResponse'=>$output];
                 $result = ['content' => $content, 'code' => $code];
             } else if ($action == 'decrypt' && ctype_xdigit($string)) {
                 $output = openssl_decrypt(hex2bin($string), $encrypt_method, $secret_key, OPENSSL_RAW_DATA, $secret_iv);
                 $code = 200;            
-                $content =  ['status'=>$code,  "httpStatus"=> $code,"codeDescription"=> __("Success"),  'apiResponse'=>$output];
+                $content =  ['status'=>$code,  "httpStatus"=> $code,"codeDescription"=> "Success",  'apiResponse'=>$output];
                 $result = ['content' => $content, 'code' => $code];
             } else{
                 $code = 401;
-                $msg = __('Invalid parameters');        
-                $content =  ['status'=>$code,"httpStatus"=> $code,"codeDescription"=> __("Failed"), "message"=>  __($msg)];
+                $msg = 'Invalid parameters';        
+                $content =  ['status'=>$code,"httpStatus"=> $code,"codeDescription"=> "Failed", "message"=> $msg];
                 $result = ['content' => $content, 'code' => $code];
             }
         } catch(Exception $e){
@@ -34,8 +34,8 @@ class EncryptDecrypt
         }
         if(empty($output)){
             $code = 402;
-            $msg = __('Invalid param string');      
-            $content =  ['status'=>$code,"httpStatus"=> $code,"codeDescription"=> __("Failed"), "message"=>  __($msg)];
+            $msg = 'Invalid param string';      
+            $content =  ['status'=>$code,"httpStatus"=> $code,"codeDescription"=> "Failed", "message"=>  $msg];
             $result = ['content' => $content, 'code' => $code];
         }
         return $result;
